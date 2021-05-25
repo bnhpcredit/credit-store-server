dc1f55ae95a92eccfa339daa3fd21b738faf2f2a
Dani700 <reze.dani@gmail.com>
2021-05-25 09:30:32 +0300

    springified db config and made repo method generic

--- src/main/java/com/example/demo/repo/MongoRepo.java
+++ src/main/java/com/example/demo/repo/MongoRepo.java
@@ -1,74 +1,31 @@
 package com.example.demo.repo;
-import com.example.demo.data.CreditLimitOffer;
-import com.example.demo.data.DigitalGuaranteeOffer;
-import com.example.demo.data.LoanOffer;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 
 import org.bson.Document;
-import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.codecs.pojo.PojoCodecProvider;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
-import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 
-@Component
+@Repository
 public class MongoRepo {
 
-    @Value("${mongodb.uri}")
-	private String mongoConnectionUri;
 
-    private CodecRegistry pojoCodecRegistry;
-    private MongoClient mongoClient;
+    @Autowired
     private MongoDatabase database;
 
+    public <T> List<T> getGeneric(String collection, Class clazz){
 
-    //must springify this class
-    private void initMongoConnection(){
-        pojoCodecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), fromProviders(PojoCodecProvider.builder().automatic(true).build()));
-		mongoClient = MongoClients.create(mongoConnectionUri);
-	    database = mongoClient.getDatabase("Products").withCodecRegistry(pojoCodecRegistry);
-
+        MongoCollection<T> loanCollection = database.getCollection(collection, clazz);
+		Collection<T> coll= loanCollection.find(new Document(), clazz).into(new ArrayList());
+        return new ArrayList<T>(coll);
     }
 
-    //must make generic
 
-    public List<LoanOffer> getLoanOfferCollection(){
-        initMongoConnection();
-
-        MongoCollection<LoanOffer> loanCollection = database.getCollection("LoanOffers", LoanOffer.class);
-		List<LoanOffer> loanOffers = loanCollection.find(new Document(), LoanOffer.class).into(new ArrayList<LoanOffer>());
-        return loanOffers;
-
-    }
-
-    public List<DigitalGuaranteeOffer> getDigitalGuaranteeCollection(){
-        initMongoConnection();
-
-        MongoCollection<DigitalGuaranteeOffer> digitalGuaranteeOffersCollection = database.getCollection("DigitalGuaranteeOffers", DigitalGuaranteeOffer.class);
-		List<DigitalGuaranteeOffer> digitalGuaranteeOffers = digitalGuaranteeOffersCollection.find(new Document(), DigitalGuaranteeOffer.class).into(new ArrayList<DigitalGuaranteeOffer>());
-        return digitalGuaranteeOffers;
-
-    }
-
-    public List<CreditLimitOffer> getCreditLimitCollection(){
-        initMongoConnection();
-
-        MongoCollection<CreditLimitOffer> creditLimitOfferscollection = database.getCollection("CreditLimitOffers", CreditLimitOffer.class);
-		List<CreditLimitOffer> creditLimitOffers = creditLimitOfferscollection.find(new Document(), CreditLimitOffer.class).into(new ArrayList<CreditLimitOffer>());
-        return creditLimitOffers;
-
-    }
 
 
     
--- src/main/java/com/example/demo/service/ProductsServiceImpl.java
+++ src/main/java/com/example/demo/service/ProductsServiceImpl.java
@@ -1,35 +1,21 @@
 package com.example.demo.service;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
 
-import com.example.demo.data.CardOffer;
+import java.util.List;
+
 import com.example.demo.data.CreditLimitOffer;
 import com.example.demo.data.DigitalGuaranteeOffer;
 import com.example.demo.data.LoanOffer;
 import com.example.demo.data.ProductOffers;
 import com.example.demo.repo.MongoRepo;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoDatabase;
 
-import org.bson.Document;
-import org.bson.codecs.configuration.CodecRegistry;
-import org.bson.codecs.pojo.PojoCodecProvider;
-import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
-import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Component
+@Service
 public class ProductsServiceImpl implements ProductsService {
 
 	@Value("${mongodb.uri}")
@@ -38,13 +24,12 @@ public class ProductsServiceImpl implements ProductsService {
 	@Autowired
 	private MongoRepo repo;
 
-	//must springify repo to make it more generic
     public ProductOffers getProducts(@RequestParam Long partyId) {
 		ProductOffers productOffers = new ProductOffers();
 		
-		List<LoanOffer> loanOffers = repo.getLoanOfferCollection();
-		List<DigitalGuaranteeOffer> digitalGuaranteeOffers = repo.getDigitalGuaranteeCollection();
-		List<CreditLimitOffer> creditLimitOffers = repo.getCreditLimitCollection();
+		List<LoanOffer> loanOffers = repo.getGeneric("LoanOffers", LoanOffer.class);
+		List<DigitalGuaranteeOffer> digitalGuaranteeOffers = repo.getGeneric("DigitalGuaranteeOffers", DigitalGuaranteeOffer.class);
+		List<CreditLimitOffer> creditLimitOffers = repo.getGeneric("CreditLimitOffers", CreditLimitOffer.class);
 
 		productOffers.setLoanOffers(loanOffers);
 		productOffers.setDigitalGuaranteeOffers(digitalGuaranteeOffers);
